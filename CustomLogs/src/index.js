4440c1fbb65133f6be9228bc83b14dde6731064a
António Pereira <a18809@alunos.ipca.pt>
2022-11-15 22:41:34 +0000

    Added first lines of log
    First attempt with the basic log

--- CustomLogs/src/index.js
+++ CustomLogs/src/index.js
@@ -1,15 +1,35 @@
-const express = require('express');
+const express = require('express')
+const morgan = require('morgan')
+const winston = require('winston')
 
 // Constants
-const PORT = 8080;
-const HOST = '0.0.0.0';
+const PORT = 8082
+const HOST = '0.0.0.0'
+
+let logger = winston.createLogger({
+  transports: [
+    new (winston.transports.Console)(),
+    new (winston.transports.File)({ filename: 'app.log'})
+  ], 
+  exitOnError: false,
+  level: 'info'
+})
+
+const myStream = {
+  write: (text) => {
+    logger.info(text)
+  }
+}
+
 
 // App
-const app = express();
+const app = express()
 app.get('/', (req, res) => {
-  res.send('Hello World');
-});
+  res.send('Hello World')
+})
+app.use(morgan('combined', {stream: myStream}))
+//app.use(morgan('tiny'))
 
 app.listen(PORT, HOST, () => {
-  console.log(`Running on http://${HOST}:${PORT}`);
-});
+  console.log(`Running on http://${HOST}:${PORT}`)
+})
